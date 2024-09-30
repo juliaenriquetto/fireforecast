7c2fe366f9d136dab5f115128a1bd576c6e6ef6b
Julia Enriquetto de Brito <juliaenriquettodebrito@gmail.com>
2024-09-30 18:13:35 -0300

    feat: add 2020 data

--- model/additional_data/cleaning_data/MeteorologicalDataSummarizer.py
+++ model/additional_data/cleaning_data/MeteorologicalDataSummarizer.py
@@ -68,6 +68,6 @@ def process_data(input_file, output_file):
         print(f"Ocorreu um erro: {e}")
 
 
-input_file = 'INMET_SE_SP_BAURU_2019_original.csv'  # Substitua pelo caminho do arquivo CSV original
-output_file = 'INMET_SE_SP_BAURU_2019.csv'  # Nome do novo arquivo CSV
+input_file = 'INMET_CO_GO_GOIANIA_2020_original.csv'  # Substitua pelo caminho do arquivo CSV original
+output_file = 'INMET_CO_GO_GOIANIA_2020.csv'  # Nome do novo arquivo CSV
 process_data(input_file, output_file)
