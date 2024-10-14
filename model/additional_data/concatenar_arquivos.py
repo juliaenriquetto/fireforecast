0fdeed41b3b03e06d9af06c3d72b0a7a7b34a383
Júlia Enriquetto de Brito <107726291+juliaenriquetto@users.noreply.github.com>
2024-10-14 11:46:29 -0300

    feat: add concatenar_arquivos

---
+++ model/additional_data/concatenar_arquivos.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import glob # carregar todos os arquivos de uma vez
+
+# caminho onde estão armazenados os CSVs
+path = '2019/*.csv'
+
+# carregar todos os arquivos CSV da pasta
+arquivos_csv = glob.glob(path)
+
+# ler e concatenar os dataframes 
+df_list = [pd.read_csv(arquivo) for arquivo in arquivos_csv]
+df_completo = pd.concat(df_list, ignore_index=True)
+
+# salvar o dataframe concatenado em um novo arquivo CSV
+df_completo.to_csv('2019/df_2019.csv', index=False)
+
+# exibir as primeiras linhas do dataframe concatenado
+print("Dados concatenados salvos com sucesso!")
