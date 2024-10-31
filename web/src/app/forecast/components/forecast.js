a2f30fb2a69eec547a38d033dfa6a43bb0c1006d
Júlia Enriquetto de Brito <107726291+juliaenriquetto@users.noreply.github.com>
2024-10-31 14:15:52 -0300

    feat(web): add map

--- web/src/app/forecast/components/forecast.js
+++ web/src/app/forecast/components/forecast.js
@@ -1,4 +1,7 @@
-import Layout from '@/components/layout'
+"use client"
+
+import Layout from '@/components/layout';
+import Model from '@/app/forecast/components/model/map'
 
 export default function Forecast() {
   return (
@@ -7,9 +10,9 @@ export default function Forecast() {
         <main className="relative flex w-full flex-col items-center justify-center gap-y-10 text-lg font-normal md:w-4/5 md:justify-between">
           <div className="flex flex-col items-center justify-start gap-y-10 sm:gap-y-20">
             <div className="flex h-full w-full flex-col items-center justify-start gap-y-1 px-8 md:max-w-lg lg:max-w-2xl">
-                <h2 className="font-sans-heading from-foreground to-muted-foreground bg-gradient-to-r bg-clip-text text-2xl font-semibold tracking-tighter text-transparent sm:text-3xl xl:text-4xl/none">
-                  Previsão
-                </h2>
+              <article className="flex w-full flex-col items-start gap-y-6 pb-10 md:w-[40rem] lg:w-[50rem]">
+                <Model />
+              </article>
             </div>
           </div>
         </main>
---
+++ web/src/app/forecast/components/model/map.js
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+export default function MapComponent() {
+    const mapRef = useRef(null);
+
+    useEffect(() => {
+        // Verifique se o mapa já foi criado
+        if (mapRef.current === null) {
+            mapRef.current = L.map('map').setView([-23.5505, -46.6333], 10); // ref.: loc de sp
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '© OpenStreetMap contributors'
+            }).addTo(mapRef.current);
+        }
+
+        // Limpa o mapa quando o componente desmonta
+        return () => {
+            if (mapRef.current) {
+                mapRef.current.remove();
+                mapRef.current = null;
+            }
+        };
+    }, []);
+
+    return (
+        <div className="flex justify-start w-full">
+            <div id="map" className="h-[500px] w-[80%] mr-auto" style={{ height: '500px', width: '100%' }}></div>
+        </div>
+    );
+}
