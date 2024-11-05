28e5a17df87d22912f61981c0acabe8a5dc95bb6
Clara Andrade <113606213+claraandradee@users.noreply.github.com>
2024-11-05 14:53:39 -0300

    style(web): add logo

--- web/src/components/fireforecast-heading.js
+++ web/src/components/fireforecast-heading.js
@@ -1,7 +1,7 @@
 export default function FireforecastHeading() {
     return (
       <h1 className="text-primary font-sans-heading font-medium flex items-center">
-        <img src="/path/to/your/logo.png" alt="Logo" className="mr-2 h-8 w-8" /> 
+        <img src="./public/logo.png" alt="Logo" className="mr-2 h-8 w-8" /> 
         Fireforecast
       </h1>
     );
