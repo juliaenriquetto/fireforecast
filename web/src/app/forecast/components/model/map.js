6be1190dfe385f8fc3d2170f3398987067dbe424
Clara Andrade <113606213+claraandradee@users.noreply.github.com>
2024-11-05 21:29:11 -0300

    Update[web]: map.js

--- web/src/app/forecast/components/model/map.js
+++ web/src/app/forecast/components/model/map.js
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef } from 'react';
 import L from 'leaflet';
+import Papa from 'papaparse';
 import 'leaflet/dist/leaflet.css';
 
 export default function MapComponent() {
@@ -17,6 +18,9 @@ export default function MapComponent() {
             }).addTo(mapRef.current);
         }
 
+        // Carrega os dados do CSV e adiciona bolinhas
+        loadMarkersFromCSV();
+
         // Limpa o mapa quando o componente desmonta
         return () => {
             if (mapRef.current) {
@@ -25,6 +29,50 @@ export default function MapComponent() {
             }
         };
     }, []);
+
+    const loadMarkersFromCSV = async () => {
+        const csvUrl = '/dados.csv'; // Ajuste o caminho para o seu arquivo CSV
+    
+        const response = await fetch(csvUrl);
+        const csvText = await response.text();
+    
+        Papa.parse(csvUrl, {
+            download: true,
+            header: true,
+            complete: function (results) {
+              console.log("Dados lidos do CSV:", results.data);
+              results.data.forEach((row) => {
+                const latitude = parseFloat(row.latitude);
+                const longitude = parseFloat(row.longitude);
+                const probabilidade = parseFloat(row.probabilidade);
+          
+                // Verifica se os valores são válidos antes de criar o marcador
+                if (!isNaN(latitude) && !isNaN(longitude) && !isNaN(probabilidade)) {
+                  // Define a cor de acordo com os novos intervalos de probabilidade
+                  let color;
+                  if (probabilidade <= 25) {
+                    color = 'lightyellow';
+                  } else if (probabilidade > 25 || probabilidade <= 50) {
+                    color = 'gold';
+                  } else if (probabilidade > 50 || probabilidade <= 75) {
+                    color = 'orange';
+                  } else if (probabilidade > 75 || probabilidade <= 85) {
+                    color = 'red';
+                  }
+          
+                  // Adiciona o círculo ao mapa com a cor e raio definidos
+                  L.circle([latitude, longitude], {
+                    color,
+                    radius: 5000,
+                    fillOpacity: 0.5,
+                  }).addTo(mapRef.current);
+                } else {
+                  console.error("Valores inválidos:", { latitude, longitude, probabilidade });
+                }
+              });
+            },
+          });          
+    };
 
     return (
         <div className="flex justify-start w-full">
